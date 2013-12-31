1d0b22d412a7fb3bec28acf0d9a8e965cae86b8c
lifo <lifo.java@yahoo.com>
2013-12-31 17:07:06 +0600

    test

--- Controller.php
+++ Controller.php
@@ -3,21 +3,12 @@ function __autoload($className){
 	include_once("models/$className.php");	
 }
 
-//$users=new User("your_host","your_user","your_password","your_database");
-$users=new User("tunnel.pagodabox.com","leann","LFmskHzx","crud");
-//$users=new User("localhost","root","root","test");
+$users=new User("your_host","your_user","your_password","your_database");
 
 if(!isset($_POST['action'])) {
 	print json_encode(0);
 	return;
 }
-//$userParams = $_POST['user'];
-// Almost spoiled my day 11th May 2013.
-/*if(get_magic_quotes_gpc()){
-    $userParams = stripslashes($_POST['user']);
-} else {
-    $userParams = $_POST['user'];
-}*/
 
 switch($_POST['action']) {
 	case 'get_users':
